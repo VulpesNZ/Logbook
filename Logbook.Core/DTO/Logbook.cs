3e0199c786fd4de70be81038cb5b471b1e25a832
Andrew Smith <vulpesnz@gmail.com>
2017-02-21 14:44:32 +1300

    Create and view logbooks stub

---
+++ Logbook.Core/DTO/Logbook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logbook.Core.DTO
+{
+    public class LogbookDTO
+    {
+        public Guid LogbookId { get; set; }
+        public Guid IndustryId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public Guid DefaultActivityId { get; set; }
+    }
+}
---
+++ LogbookUI/Models/LogbookViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Logbook.Core.DTO;
+
+namespace LogbookUI.Models
+{
+    public class CreateLogbookViewModel
+    {
+        [Required]
+        [Display(Name = "Logbook Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Default Activity")]
+        public Guid DefaultActivityId { get; set; }
+        
+        public List<ActivityDTO> Activities { get; set; }
+    }
+
+    public class LogbookViewModel
+    {
+        public Guid LogbookId { get; set; }
+
+        [Display(Name = "Logbook Name")]
+        public string Name { get; set; }
+    }
+}
